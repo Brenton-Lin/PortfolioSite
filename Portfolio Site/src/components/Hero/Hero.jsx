01aa8f1e7345bee1a96d72cd6c7d75e4808f22bf
Brenton-Lin <120334846+Brenton-Lin@users.noreply.github.com>
2024-05-11 18:54:26 -0400

    Thumbnails, Gradients on titles, project info updates and links

--- Portfolio Site/src/components/Hero/Hero.jsx
+++ Portfolio Site/src/components/Hero/Hero.jsx
@@ -8,10 +8,12 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Brenton</h1>
-        <p className={styles.description}>
-          I'm a software developer with 2 years of experience building and supporting multiplayer training software in the Unity game engine. 
-          
-        </p>
+          <p className={styles.description}>
+            I'm a software developer with 2 years of experience building and supporting multiplayer, virtual-reality training software in the Unity game engine.  
+          </p>
+          <p className={styles.description}>
+            I love thinking up creative solutions to functional problems. Also a general tech/fantasy nerd, ask me what I'm currently reading when you reach out!
+          </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
