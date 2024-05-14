b00d15bc9725f89f8478cc5b859c8e3b1e5fc9e4
Brenton-Lin <120334846+Brenton-Lin@users.noreply.github.com>
2024-05-14 01:36:55 -0400

    minor content updates

--- Portfolio Site/src/components/Hero/Hero.jsx
+++ Portfolio Site/src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { TextLoop } from "easy-react-text-loop";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -8,12 +8,14 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Brenton</h1>
-          <p className={styles.description}>
-            I'm a software developer with 2 years of experience building and supporting multiplayer, virtual-reality training software in the Unity game engine.  
-          </p>
-          <p className={styles.description}>
-            I love thinking up creative solutions to functional problems. Also a general tech/fantasy nerd, ask me what I'm currently reading when you reach out!
-          </p>
+          
+            <p className={styles.description}>
+              I'm a software developer with 2 years of experience building and supporting multiplayer, virtual-reality training software in the Unity game engine.  
+            </p>
+            <p className={styles.description}>
+              Also a general tech/fantasy nerd, ask me what I'm currently reading when you reach out!
+            </p>
+         
         <a href="mailto:brenton.c.lin@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
